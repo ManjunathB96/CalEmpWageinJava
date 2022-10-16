4da1d2ffe718f14197c62bf467f162bd7b6d155f
Manju31051996 <bbelagavi6@gmail.com>
2022-10-17 00:28:26 +0530

    [Manjunath] added the EmployeeUC4 file

---
+++ src/com/EmpwgeCalculation/EmployeeUC4.java
@@ -0,0 +1,27 @@
+package com.EmpwgeCalculation;
+
+public class EmployeeUC4 {
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+    public static final int EMP_RATE_PER_HR = 20;
+
+    public static void main(String[] args) {
+        int empHrs = 0;
+        int empWage = 0;
+
+        int empCheck = (int) Math.floor(Math.random() * 10) %3;
+
+        switch ( empCheck ) {
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            default:
+                empHrs = 0;
+        }
+        empWage = empHrs * EMP_RATE_PER_HR;
+        System.out.println("Wage of Employee is :"+empWage);
+    }
+}
