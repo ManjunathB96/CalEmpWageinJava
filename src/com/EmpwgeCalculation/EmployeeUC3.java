9df494b2092d89f877e10cad6001de21cb2ba842
Manju31051996 <bbelagavi6@gmail.com>
2022-10-17 00:15:37 +0530

    Add EmployeeUC3 file

---
+++ src/com/EmpwgeCalculation/EmployeeUC3.java
@@ -0,0 +1,25 @@
+package com.EmpwgeCalculation;
+
+public class EmployeeUC3 {
+    public static void main(String[] args) {
+        int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 1;
+        int EMP_RATE_PER_HR = 20;
+
+        int empHrs = 0;
+        int empWage = 0;
+
+        double empCheck = Math.floor((Math.random()*10) %3);
+        if (empCheck == IS_PART_TIME){
+            empHrs = 4;
+        }
+        else if (empCheck == IS_FULL_TIME){
+            empHrs = 8;
+        }
+        else {
+            empHrs =0;
+        }
+        empWage = empHrs * EMP_RATE_PER_HR;
+        System.out.println("Emplyee is FULL TIME or PART TIME :"+empWage);
+    }
+}
