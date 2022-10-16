453132a346fc156adc4c38902a7e3e79d32a4b06
Manju31051996 <bbelagavi6@gmail.com>
2022-10-17 00:25:35 +0530

    Change the file

--- src/com/EmpwgeCalculation/EmployeeUC3.java
+++ src/com/EmpwgeCalculation/EmployeeUC3.java
@@ -9,17 +9,17 @@ public class EmployeeUC3 {
         int empHrs = 0;
         int empWage = 0;
 
-        double empCheck = Math.floor((Math.random() * 10) % 3);
-        if (empCheck == IS_PART_TIME) {
+        double empCheck = Math.floor((Math.random()*10) %3);
+        if (empCheck == IS_PART_TIME){
             empHrs = 4;
-        } else if (empCheck == IS_FULL_TIME) {
-            empHrs = 8;
-
-        else{
-                empHrs = 0;
-            }
-            empWage = empHrs * EMP_RATE_PER_HR;
-            System.out.println("Emplyee is FULL TIME or PART TIME :" + empWage);
         }
+        else if (empCheck == IS_FULL_TIME){
+            empHrs = 8;
+        }
+        else {
+            empHrs =0;
+        }
+        empWage = empHrs * EMP_RATE_PER_HR;
+        System.out.println("Emplyee is FULL TIME or PART TIME :"+empWage);
     }
 }
